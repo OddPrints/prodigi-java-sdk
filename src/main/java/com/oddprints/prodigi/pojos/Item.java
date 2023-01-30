db8564756d0fdcf6b858b64af3310bcf5cc4cdcb
oddkarl <karl@oddprints.com>
2023-01-30 23:01:46 +0000

    Update Item.java
    Spotless check adjustments.

--- src/main/java/com/oddprints/prodigi/pojos/Item.java
+++ src/main/java/com/oddprints/prodigi/pojos/Item.java
@@ -2,7 +2,6 @@ package com.oddprints.prodigi.pojos;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
